6c6a30393890d64305a88e292768032e190a4327
David Jacqua <oshaneal@gmail.com>
2021-10-20 19:05:54 -0400

    Etape 1 : Démarrage du serveur backend + Connection à la bdd MongoDb : SUCCESS

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+//Connection à la base de donnée MongoDb
+mongoose.connect('mongodb+srv://userP6:owPKVQSH9LT9YkgH@apiavisgastro.nesyz.mongodb.net/ApiAvisGastro?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+export default app;
