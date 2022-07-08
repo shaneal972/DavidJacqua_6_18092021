a57101e1354dffba5dddfc8d7b31bac75e0e848c
David Jacqua <oshaneal@gmail.com>
2022-07-08 04:47:02 -0400

    auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, process.env.TOKEN);
+        //Récupération de l'userId qui est dans le token
         const userId = decodedToken.userId;
 
         if (req.body.userId && req.body.userId !== userId) {
